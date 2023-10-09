45c53c2f20f6eb4976e0705fcb7291199ea1b21c
Patrick Yi <21299683+pjyi2147@users.noreply.github.com>
2023-10-09 09:33:03 +0900

    Project1 alarm clock implementation (#19)
    * what's stropts.h?
    
    * i want these three files
    
    * we do not need object files
    
    * Add task and launch files for debugger attach for both bochs and gdb
    
    * Priority Scheduler
    
    * alarm clock with sleep
    
    ---------
    
    Co-authored-by: Jeongseop Yi <jeongseopyi@postech.ac.kr>

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -28,6 +28,9 @@ static struct list ready_list;
    when they are first scheduled and removed when they exit. */
 static struct list all_list;
 
+/* List of sleeping processes */
+static struct list sleep_list;
+
 /* Idle thread. */
 static struct thread *idle_thread;
 
@@ -92,6 +95,7 @@ thread_init (void)
   lock_init (&tid_lock);
   list_init (&ready_list);
   list_init (&all_list);
+  list_init (&sleep_list);
 
   /* Set up a thread structure for the running thread. */
   initial_thread = running_thread ();
@@ -258,6 +262,49 @@ thread_unblock (struct thread *t)
   ASSERT (t->status == THREAD_BLOCKED);
   list_insert_ordered (&ready_list, &t->elem, thread_priority_desc, NULL);
   t->status = THREAD_READY;
+  intr_set_level (old_level);
+}
+
+/* Set current thread to sleep until ticks and blocks it */
+void
+thread_sleep (int64_t ticks)
+{
+  struct thread *t;
+
+  enum intr_level old_level = intr_disable ();
+  t = thread_current ();
+
+  // idle thread must not sleep
+  ASSERT(t != idle_thread);
+
+  t->wakeup_ticks = ticks;
+  list_push_back (&sleep_list, &t->elem);
+  thread_block ();
+
+  intr_set_level (old_level);
+}
+
+/* Wakes up all sleeping threads before ticks and unblocks them */
+void
+thread_wake (int64_t ticks)
+{
+  struct list_elem *e;
+  struct thread *t;
+
+  enum intr_level old_level = intr_disable ();
+
+  for (e = list_begin (&sleep_list); e != list_end (&sleep_list);)
+  {
+    t = list_entry (e, struct thread, elem);
+    if (t->wakeup_ticks <= ticks)
+      {
+        e = list_remove (e);
+        thread_unblock (t);
+      }
+    else
+      break;
+  }
+
   intr_set_level (old_level);
 }
 
