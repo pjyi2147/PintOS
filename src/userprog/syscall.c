b03c317fdba98ad00c70ec7aa5f228918c2ef171
Jeongseop Yi (49004543) <jeongseopyi@postech.ac.kr>
2023-11-04 01:11:23 +0900

    minimal required for arg processing

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -7,14 +7,30 @@
 static void syscall_handler (struct intr_frame *);
 
 void
-syscall_init (void) 
+syscall_init (void)
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void
-syscall_handler (struct intr_frame *f UNUSED) 
+void
+syscall_exit (int status)
 {
-  printf ("system call!\n");
+  printf ("(%s): exit(%d)\n", thread_current()->name, status);
+  thread_exit ();
+}
+
+static void
+syscall_handler (struct intr_frame *f UNUSED)
+{
+  printf ("system call! %d \n", f->eax);
+  switch (f->eax)
+  {
+  case SYS_EXIT:
+    syscall_exit(f->edi);
+    break;
+
+  default:
+    break;
+  }
   thread_exit ();
 }
