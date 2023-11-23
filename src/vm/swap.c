625538d8aa00c71f9b13048ea63f51a53fffddb9
Jeongseop Yi (49004543) <jeongseopyi@postech.ac.kr>
2023-11-23 23:37:21 +0900

    swap structure

--- src/vm/swap.c
+++ src/vm/swap.c
@@ -1,1 +1,63 @@
 #include "vm/swap.h"
+#include <bitmap.h>
+#include "threads/vaddr.h"
+#include "threads/synch.h"
+#include "userprog/syscall.h"
+
+#define SECTORS_PER_PAGE (PGSIZE / BLOCK_SECTOR_SIZE)
+
+static struct bitmap *swap_table;
+static struct block *swap_block;
+static struct lock swap_lock;
+
+void swap_table_init(void)
+{
+  swap_block = block_get_role (BLOCK_SWAP);
+  swap_table = bitmap_create(block_size(swap_block) / SECTORS_PER_PAGE);
+
+  bitmap_set_all(swap_table, false);
+  lock_init(&swap_lock);
+}
+
+void swap_in(struct page *p, void *kva)
+{
+  int i;
+  int swap_index = p->swap_index;
+
+  lock_acquire (&swap_lock);
+  if (swap_index > bitmap_size (swap_table) || swap_index < 0)
+  {
+    syscall_exit (-1);
+  }
+
+  if (bitmap_test (swap_table, swap_index) == false)
+  {
+    syscall_exit (-1);
+  }
+
+  bitmap_set (swap_table, swap_index, false);
+
+  lock_release (&swap_lock);
+
+  for (i = 0; i < SECTORS_PER_PAGE; i++)
+  {
+    block_read (swap_block, swap_index * SECTORS_PER_PAGE + i, kva + i * BLOCK_SECTOR_SIZE);
+  }
+}
+
+int swap_out(void *kva)
+{
+  int i;
+  int swap_index;
+
+  lock_acquire(&swap_lock);
+  swap_index = bitmap_scan_and_flip (swap_table, 0, 1, true);
+  lock_release(&swap_lock);
+
+  for (i = 0; i < SECTORS_PER_PAGE; i++)
+  {
+    block_write (swap_block, swap_index * SECTORS_PER_PAGE + i, kva + i * BLOCK_SECTOR_SIZE);
+  }
+
+  return swap_index;
+}
