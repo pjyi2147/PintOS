704f4f8cee1b49f1250067bc3ef1e1148db156fa
Jeongseop Yi (49004543) <jeongseopyi@postech.ac.kr>
2023-11-23 23:08:36 +0900

    struct frame

--- src/vm/frame.c
+++ src/vm/frame.c
@@ -1,12 +1,12 @@
 #include "vm/frame.h"
 #include "threads/malloc.h"
-#include "threads/palloc.h"
-#include "threads/thread.h"
-#include "threads/vaddr.h"
-#include "userprog/pagedir.h"
+#include "threads/synch.h"
+#include "vm/page.h"
+#include "vm/swap.h"
 
 static struct list frame_table;
 static struct lock frame_lock;
+static struct frame *last_frame;
 
 void
 frame_init(void)
@@ -18,17 +18,24 @@ frame_init(void)
 void *
 frame_alloc(enum palloc_flags flags, void *upage)
 {
-  void *frame = palloc_get_page(flags);
-  if (frame == NULL)
+  lock_acquire(&frame_lock);
+  void *kpage = palloc_get_page(flags);
+  if (kpage == NULL)
   {
-    frame = frame_evict(flags, upage);
+    frame_evict(flags, upage);
+    kpage = palloc_get_page(flags);
+    if (kpage == NULL)
+    {
+      return NULL;
+    }
   }
   struct frame *f = malloc(sizeof(struct frame));
-  f->kpage = frame;
+  f->kpage = kpage;
   f->upage = upage;
-  f->thread = thread_current();
+  f->thread = thread_current ();
   list_push_back(&frame_table, &f->elem);
-  return frame;
+  lock_release(&frame_lock);
+  return kpage;
 }
 
 void
@@ -36,32 +43,69 @@ frame_free(void *frame)
 {
   struct list_elem *e;
   struct frame *fe;
+  lock_acquire(&frame_lock);
   for (e = list_begin(&frame_table); e != list_end(&frame_table); e = list_next(e))
   {
     fe = list_entry(e, struct frame, elem);
     if (fe->kpage == frame)
     {
       list_remove(e);
-      free(fe);
       palloc_free_page(frame);
+      pagedir_clear_page(fe->thread->pagedir, fe->upage);
+      free(fe);
+      lock_release(&frame_lock);
       return;
     }
   }
+  syscall_exit(-1);
 }
 
-void
-frame_free_all(struct thread *t)
+struct frame *
+frame_get (void *kpage)
 {
   struct list_elem *e;
   struct frame *fe;
   for (e = list_begin(&frame_table); e != list_end(&frame_table); e = list_next(e))
   {
     fe = list_entry(e, struct frame, elem);
-    if (fe->thread == t)
+    if (fe->kpage == kpage)
     {
-      list_remove(e);
-      free(fe);
-      palloc_free_page(fe->kpage);
+      return fe;
     }
   }
+  return NULL;
+}
+
+void
+frame_evict(enum palloc_flags flags, void *upage)
+{
+  ASSERT(lock_held_by_current_thread(&frame_lock));
+
+  struct frame *fe = last_frame;
+  struct page *pe;
+
+  do
+  {
+    if (fe != NULL)
+    {
+      pagedir_set_accessed(fe->thread->pagedir, fe->upage, false);
+    }
+
+    if (last_frame == NULL || list_next(&fe->elem) == list_end(&frame_table))
+    {
+      fe = list_entry(list_begin(&frame_table), struct frame, elem);
+    }
+    else
+    {
+      fe = list_entry(list_next(&fe->elem), struct frame, elem);
+    }
+  } while (!pagedir_is_accessed(fe->thread->pagedir, fe->upage));
+
+  // TODO: swap out the frame
+  pe = page_get (fe->thread->page_table, fe->upage);
+  pe->status = PAGE_STATUS_SWAP;
+  pe->swap_index = swap_out(fe->kpage);
+
+  frame_free(fe->kpage);
+  lock_release(&frame_lock);
 }
