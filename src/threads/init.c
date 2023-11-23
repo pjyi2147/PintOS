1f1ca332650a01ff0b576c6e0ebf43ed815026f3
Jeongseop Yi (49004543) <jeongseopyi@postech.ac.kr>
2023-11-24 00:29:00 +0900

    stack growth

--- src/threads/init.c
+++ src/threads/init.c
@@ -39,6 +39,7 @@
 #endif
 #ifdef VM
 #include "vm/frame.h"
+#include "vm/swap.h"
 #endif
 
 /* Page directory with kernel mappings only. */
@@ -132,6 +133,7 @@ main (void)
 
 #ifdef VM
   frame_init();
+  swap_table_init();
 #endif
 
   printf ("Boot complete.\n");
